349051369d6c49cf18ebaca5e095e2984a62ca89
윤종서 <jongseoy2@gamil.com>
2021-04-12 15:54:47 +0900

    process update

--- process.go
+++ process.go
@@ -70,7 +70,7 @@ func (p Process) Spawn(path, URI string) *exec.Cmd {
 	}
 	processCommands = append(processCommands,
 		"-hls_flags",
-		p.getHLSFlags(),
+		"flush_packets",
 		"-f",
 		"hls",
 		"-segment_list_flags",
